2b61d145a1f40a4573f7d184432cda87189c8237
Rishav Thakker <thakker.rishav@gmail.com>
2022-07-23 22:16:30 +0530

    changed region to mumbai

--- aws-cdk/bin/main.ts
+++ aws-cdk/bin/main.ts
@@ -5,5 +5,5 @@ import { DBStack } from "../lib/db";
 const app = new cdk.App();
 
 new DBStack(app, "DBStack", {
-  env: { account: "628038590822", region: "us-east-2" },
+  env: { account: "628038590822", region: "ap-south-1" },
 });
