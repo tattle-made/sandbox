891f5e6aefe0a0d11be6139d87022e6abe47b71f
dennyabrain <denny.george90@gmail.com>
2022-10-02 16:52:30 +0530

    chore: testing ci

--- debug-container-node/index.js
+++ debug-container-node/index.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/info", (req, res) => {
+  res.json({ version: process.env.COMMIT_ID });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`version : ${process.env.COMMIT_ID}`);
 });
